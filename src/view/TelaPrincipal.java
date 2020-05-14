149aa201d032185f11c1a5346e49cae69fefcc70
Abner Matias <abnermatias2201@outlook.com>
2020-05-14 16:57:21 -0300

    Listar Usuarior
    Correção nos SQL da tela usuarios

--- src/view/TelaPrincipal.java
+++ src/view/TelaPrincipal.java
@@ -359,7 +359,7 @@ public class TelaPrincipal extends JFrame {
                     	 tempo.setSeconds(tempo.getSeconds()+1);
                     	 rodapeText.setText("User: "+ s.getNome()+"               "
                     	 		+ "      "+"Tempo de sessão: "+format.format(tempo)
-                    	 +"                " + " (Versão 1.5.6.1)");
+                    	 +"                " + " (Versão 1.6)");
                            
                    } catch (Exception e) {      
                          e.printStackTrace();      
